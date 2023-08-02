fa34346e7c528e9a02f64a992aa3dc3ff58cb582
vmelikyan <vahan.melikyan@gmail.com>
2023-08-02 16:56:18 -0700

    add __lbheartbeat__ endpoint for readinees check

--- my-express-app/app.js
+++ my-express-app/app.js
@@ -6,6 +6,9 @@ app.get('/', (req, res) => {
   res.send('Hello from lifecycle via helm!')
 })
 
+app.get('/__lbheartbeat__', (req, res) => {
+  res.send('beating heart')
+})
 app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`)
 })
